2f69b75b1c80c36412794ba38900ae352089357f
Richard Rodrigues <jrtoledorrtj@gmail.com>
2022-01-13 22:19:27 +0900

    minor changes

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ from __class_graph_analysis import GraphsAnalysis
 def parser():
     parser = argparse.ArgumentParser("IdeaFlux main function.")
 
-    parser.add_argument("--h5_dir", type=str, default="data/", help="h5 dir")
+    parser.add_argument("--h5_dir", type=str, default="data/202201/", help="h5 dir")
 
-    parser.add_argument("--res_dir", type=str, default="data/", help="results dir")
+    parser.add_argument("--res_dir", type=str, default="data/202201/", help="results dir")
     
-    parser.add_argument("--topics_df_fp", type=str, default="data/topics_df.h5", help="filepath to master h5 (dataframe when accessed)")
+    parser.add_argument("--topics_df_fp", type=str, default="data/202201/topics_df.h5", help="filepath to master h5 (dataframe when accessed)")
 
     parser.add_argument("--login_yaml_fp", type=str, default="login.yaml",
                         help="login yaml path")
 
     parser.add_argument("--subreddits", nargs="+", default=["MachineLearning", "cscareerquestions", "careerguidance", "askengineers", "jobs", "recruitinghell"], type=str, help="subrredits to be scrapped")
     
-    parser.add_argument("--save_dir", type=str, default="data/",
+    parser.add_argument("--save_dir", type=str, default="data/202201/",
                         help="folder where h5 files are going to be saved")
 
     parser.add_argument("--top_num_posts", type=int, default=5,
@@ -33,7 +33,6 @@ def main():
     args = parser()
 
     login_yaml_fp = args.login_yaml_fp
-    subreddits = args.subreddits
     save_dir = args.save_dir
     top_num_posts = args.top_num_posts
     subreddits_list = args.subreddits
@@ -47,9 +46,9 @@ def main():
     
     # # generate reddit handler
     reddit = gen_reddit(login)
-    
-    data_scraper = DataScraper(save_dir="data/", reddit_handle=reddit, subreddits_list=subreddits_list)
-    data_scraper.dl_df_routine(top_num=5)
+
+    data_scraper = DataScraper(save_dir=save_dir, reddit_handle=reddit, subreddits_list=subreddits_list)
+    data_scraper.dl_df_routine(top_num=top_num_posts)
     graphs_analysis = GraphsAnalysis(h5_dir=h5_dir, res_dir=res_dir, topics_df_fp=topics_df_fp)
     graphs_analysis.batch_summarization()
 
