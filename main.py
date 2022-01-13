e267afd88cb4900ef9041dbf7c7ac94594b05e85
Richard Rodrigues <jrtoledorrtj@gmail.com>
2022-01-13 22:07:45 +0900

    modify main & add topics_df

--- main.py
+++ main.py
@@ -47,19 +47,7 @@ def main():
     
     # # generate reddit handler
     reddit = gen_reddit(login)
-
-    # subreddits_list = list()
-    # subreddits_list.append("MachineLearning")
-    # subreddits_list.append("askscience")
-    # subreddits_list.append("productivity")
-    # subreddits_list.append("CasualConversation")
-    # subreddits_list.append("Stoicism")
-    # subreddits_list.append("optimisemylife")
-    # subreddits_list.append("EverythingScience")
     
-    # for subreddit in reddit.subreddits.default(limit=None):
-    #     top_subrredits.append(str(subreddit))
-    #     print(str(subreddit))
     data_scraper = DataScraper(save_dir="data/", reddit_handle=reddit, subreddits_list=subreddits_list)
     data_scraper.dl_df_routine(top_num=5)
     graphs_analysis = GraphsAnalysis(h5_dir=h5_dir, res_dir=res_dir, topics_df_fp=topics_df_fp)
